acf30f986e54f682a3cf6d744a2919bb0569dd89
齊藤れいら <saitoureira@ip-10-95-2-60.ap-northeast-1.compute.internal>
2024-11-14 14:27:28 +0900

    11141427

--- search_app/models.py
+++ search_app/models.py
@@ -12,6 +12,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-
+    
     def __str__(self):
         return self.name
